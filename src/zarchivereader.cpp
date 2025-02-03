86b284898da960f65facef812bee013d06080a31
YGOmod <130164085+YGOmod@users.noreply.github.com>
2025-02-04 00:43:19 +0100

    Update zarchivereader.cpp

--- src/zarchivereader.cpp
+++ src/zarchivereader.cpp
@@ -94,10 +94,10 @@ ZArchiveReader* ZArchiveReader::OpenFromStream(std::unique_ptr<std::istream>&& f
 	return cfs;
 }
 
-ZArchiveReader::ZArchiveReader(std::unique_ptr<std::ifstream> file, std::vector<_ZARCHIVE::CompressionOffsetRecord>&& offsetRecords, std::vector<uint8_t>&& nameTable, std::vector<_ZARCHIVE::FileDirectoryEntry>&& fileTree, uint64_t compressedDataOffset, uint64_t compressedDataSize)
+/*ZArchiveReader::ZArchiveReader(std::unique_ptr<std::ifstream> file, std::vector<_ZARCHIVE::CompressionOffsetRecord>&& offsetRecords, std::vector<uint8_t>&& nameTable, std::vector<_ZARCHIVE::FileDirectoryEntry>&& fileTree, uint64_t compressedDataOffset, uint64_t compressedDataSize)
     : ZArchiveReader(std::move(file), std::move(offsetRecords), std::move(nameTable), std::move(fileTree), compressedDataOffset, compressedDataSize)
 {
-}
+}*/
 
 ZArchiveReader::ZArchiveReader(std::unique_ptr<std::istream>&& file, std::vector<_ZARCHIVE::CompressionOffsetRecord>&& offsetRecords, std::vector<uint8_t>&& nameTable, std::vector<_ZARCHIVE::FileDirectoryEntry>&& fileTree, uint64_t compressedDataOffset, uint64_t compressedDataSize)
     : m_file(std::move(file)), m_offsetRecords(std::move(offsetRecords)), m_nameTable(std::move(nameTable)), m_fileTree(std::move(fileTree)),
